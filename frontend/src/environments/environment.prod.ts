2ef7bf6933f910455ab7fbe48fef341b7dbdfa96
BryanNrgPl <bryan772911@gmail.com>
2026-08-18 20:57:31 -0500

    fix: reescritura de proxy vercel para solucionar mixed content HTTP/HTTPS

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://129.80.209.22:8080/api',
-  fastApiUrl: 'http://129.80.209.22:8080/api/v1'
+  apiUrl: '/api',
+  fastApiUrl: '/api/v1'
 };
